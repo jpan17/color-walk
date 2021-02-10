5c24e05fe7e3512bad52788d005ee5ae4155fb36
Jessica Pan <jzpan17@gmail.com>
2021-02-10 10:12:53 -0500

    please wokr

--- js/scene/data.js
+++ js/scene/data.js
@@ -1,127 +1,127 @@
 data=`[
     {
       "id": 1,
-      "src": "1.jpg",
+      "src": "images/1.jpg",
       "lat": 40.344179,
       "lon": -74.654821
     },
     {
       "id": 2,
-      "src": "2.jpg",
+      "src": "images/2.jpg",
       "lat": 40.344375,
       "lon": -74.654418
     },
     {
       "id": 4,
-      "src": "4.jpg",
+      "src": "images/4.jpg",
       "lat": 40.345213,
       "lon": -74.654580
     },
     {
       "id": 5,
-      "src": "5.jpg",
+      "src": "images/5.jpg",
       "lat": 40.346910,
       "lon": -74.655487
     },
     {
       "id": 6,
-      "src": "6.jpg",
+      "src": "images/6.jpg",
       "lat": 40.347911,
       "lon": -74.654934
     },
     {
       "id": 7,
-      "src": "7.jpg",
+      "src": "images/7.jpg",
       "lat": 40.350552,
       "lon": -74.656560
     },
     {
       "id": 8,
-      "src": "8.jpg",
+      "src": "images/8.jpg",
       "lat": 40.351161,
       "lon": -74.654827
     },
     {
       "id": 9,
-      "src": "9.jpg",
+      "src": "images/9.jpg",
       "lat": 40.351814,
       "lon": -74.652731
     },
     {
       "id": 10,
-      "src": "10.jpg",
+      "src": "images/10.jpg",
       "lat": 40.351575,
       "lon": -74.653284
     },
     {
       "id": 11,
-      "src": "11.jpg",
+      "src": "images/11.jpg",
       "lat": 40.352258,
       "lon": -74.651425
     },
     {
       "id": 12,
-      "src": "12.jpg",
+      "src": "images/12.jpg",
       "lat": 40.352301,
       "lon": -74.651093
     },
     {
       "id": 13,
-      "src": "13.jpg",
+      "src": "images/13.jpg",
       "lat": 40.351215,
       "lon": -74.654611
     },
     {
       "id": 14,
-      "src": "14.jpg",
+      "src": "images/14.jpg",
       "lat": 40.351276,
       "lon": -74.654458
     },
     {
       "id": 15,
-      "src": "15.jpg",
+      "src": "images/15.jpg",
       "lat": 40.350630,
       "lon": -74.656423
     },
     {
       "id": 16,
-      "src": "16.jpg",
+      "src": "images/16.jpg",
       "lat": 40.350403,
       "lon": -74.657262
     },
     {
       "id": 17,
-      "src": "17.jpg",
+      "src": "images/17.jpg",
       "lat": 40.350211,
       "lon": -74.657495
     },
     {
       "id": 18,
-      "src": "18.jpg",
+      "src": "images/18.jpg",
       "lat": 40.350226,
       "lon": -74.657856
     },
     {
       "id": 19,
-      "src": "19.jpg",
+      "src": "images/19.jpg",
       "lat": 40.349857,
       "lon": -74.659112
     },
     {
       "id": 20,
-      "src": "20.jpg",
+      "src": "images/20.jpg",
       "lat": 40.349943,
       "lon": -74.658786
     },
     {
       "id": 21,
-      "src": "21.jpg",
+      "src": "images/21.jpg",
       "lat": 40.349585,
       "lon": -74.660167
     },
     {
       "id": 22,
-      "src": "22.jpg",
+      "src": "images/22.jpg",
       "lat": 40.348445,
       "lon": -74.661780
     }
